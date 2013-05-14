bda7ce5dd8f99e5658d30bb0dc3ca944cf372dce
dx-mon <dx-mon@users.sourceforge.net>
2013-05-14 23:44:49 +0100

    Added copyright notices to the .h files

--- gliderGun.h
+++ gliderGun.h
@@ -1,3 +1,21 @@
+/*
+ * This file is part of GOL
+ * Copyright © 2013 Rachel Mant (dx-mon@users.sourceforge.net)
+ *
+ * GOL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GOL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 void gliderGunH(uint32_t x, uint32_t y)
 {
 	set(x + 24, y + 0, true);

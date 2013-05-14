8cef93b284fa2741aa8c901cf8fe4472c8db0217
dx-mon <dx-mon@users.sourceforge.net>
2013-05-14 23:39:54 +0100

    Added creator functions for several life patterns

---
+++ gliderGun.h
@@ -0,0 +1,79 @@
+void gliderGunH(uint32_t x, uint32_t y)
+{
+	set(x + 24, y + 0, true);
+	set(x + 22, y + 1, true);
+	set(x + 24, y + 1, true);
+	set(x + 12, y + 2, true);
+	set(x + 13, y + 2, true);
+	set(x + 20, y + 2, true);
+	set(x + 21, y + 2, true);
+	set(x + 34, y + 2, true);
+	set(x + 35, y + 2, true);
+	set(x + 11, y + 3, true);
+	set(x + 15, y + 3, true);
+	set(x + 20, y + 3, true);
+	set(x + 21, y + 3, true);
+	set(x + 34, y + 3, true);
+	set(x + 35, y + 3, true);
+	set(x + 0, y + 4, true);
+	set(x + 1, y + 4, true);
+	set(x + 10, y + 4, true);
+	set(x + 16, y + 4, true);
+	set(x + 20, y + 4, true);
+	set(x + 21, y + 4, true);
+	set(x + 0, y + 5, true);
+	set(x + 1, y + 5, true);
+	set(x + 10, y + 5, true);
+	set(x + 14, y + 5, true);
+	set(x + 16, y + 5, true);
+	set(x + 17, y + 5, true);
+	set(x + 22, y + 5, true);
+	set(x + 24, y + 5, true);
+	set(x + 10, y + 6, true);
+	set(x + 16, y + 6, true);
+	set(x + 24, y + 6, true);
+	set(x + 11, y + 7, true);
+	set(x + 15, y + 7, true);
+	set(x + 12, y + 8, true);
+	set(x + 13, y + 8, true);
+}
+
+void gliderGunV(uint32_t x, uint32_t y)
+{
+	set(x + 0, y + 24, true);
+	set(x + 1, y + 22, true);
+	set(x + 1, y + 24, true);
+	set(x + 2, y + 12, true);
+	set(x + 2, y + 13, true);
+	set(x + 2, y + 20, true);
+	set(x + 2, y + 21, true);
+	set(x + 2, y + 34, true);
+	set(x + 2, y + 35, true);
+	set(x + 3, y + 11, true);
+	set(x + 3, y + 15, true);
+	set(x + 3, y + 20, true);
+	set(x + 3, y + 21, true);
+	set(x + 3, y + 34, true);
+	set(x + 3, y + 35, true);
+	set(x + 4, y + 0, true);
+	set(x + 4, y + 1, true);
+	set(x + 4, y + 10, true);
+	set(x + 4, y + 16, true);
+	set(x + 4, y + 20, true);
+	set(x + 4, y + 21, true);
+	set(x + 5, y + 0, true);
+	set(x + 5, y + 1, true);
+	set(x + 5, y + 10, true);
+	set(x + 5, y + 14, true);
+	set(x + 5, y + 16, true);
+	set(x + 5, y + 17, true);
+	set(x + 5, y + 22, true);
+	set(x + 5, y + 24, true);
+	set(x + 6, y + 10, true);
+	set(x + 6, y + 16, true);
+	set(x + 6, y + 24, true);
+	set(x + 7, y + 11, true);
+	set(x + 7, y + 15, true);
+	set(x + 8, y + 12, true);
+	set(x + 8, y + 13, true);
+}
---
+++ scrubber.h
@@ -0,0 +1,31 @@
+void scrubber(uint32_t x, uint32_t y)
+{
+	set(x + 4, y + 0, true);
+	set(x + 2, y + 1, true);
+	set(x + 3, y + 1, true);
+	set(x + 4, y + 1, true);
+	set(x + 1, y + 2, true);
+	set(x + 1, y + 3, true);
+	set(x + 4, y + 3, true);
+	set(x + 5, y + 3, true);
+	set(x + 6, y + 3, true);
+	set(x + 0, y + 4, true);
+	set(x + 1, y + 4, true);
+	set(x + 3, y + 4, true);
+	set(x + 7, y + 4, true);
+	set(x + 3, y + 5, true);
+	set(x + 7, y + 5, true);
+	set(x + 3, y + 6, true);
+	set(x + 7, y + 6, true);
+	set(x + 9, y + 6, true);
+	set(x + 10, y + 6, true);
+	set(x + 4, y + 7, true);
+	set(x + 5, y + 7, true);
+	set(x + 6, y + 7, true);
+	set(x + 9, y + 7, true);
+	set(x + 9, y + 8, true);
+	set(x + 6, y + 9, true);
+	set(x + 7, y + 9, true);
+	set(x + 8, y + 9, true);
+	set(x + 6, y + 10, true);
+}
